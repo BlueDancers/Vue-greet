07c203fcc48413d61b4db2b2b7e7fba4c426fdeb
boold <wu.vkcyan@gmail.com>
2018-04-08 20:06:21 +0800

    4-8基础模板

---
+++ src/App.vue
@@ -0,0 +1,70 @@
+<template>
+<div id="app" v-bind:title="name">
+    <!-- <comp></comp> -->
+    <img src="./assets/logo.png">
+<div>
+  <button v-on:click="additem">增加数据</button>
+</div>
+    <!-- <router-view/> -->
+    <a v-for="(item , index) in list" v-text="index+'  --------  '+item.price+'   ,   '" :key="item.name"></a>
+    <p>
+      <a v-bind:href="link">跳转</a>
+    </p>
+    <p v-if="table"> app1 </p>
+    <p v-show="!table"> app2 </p>
+    <button v-on:click="toggle">切换</button>
+</div>
+
+</template>
+
+<script>
+// import comp from './a.vue'
+export default {
+  // components: {
+  //   comp
+  // },
+  data: function () {
+    return {
+      table: true,
+      link: 'http://www.xiaowuasy.xin',
+      list: [
+        {
+          name: '1',
+          price: '1'
+        },
+        {
+          name: '2',
+          price: '2'
+        },
+        {
+          name: '3',
+          price: '3'
+        }
+      ]
+    }
+  },
+  methods: {
+    additem () {
+      this.list.push({
+        name: this.list.length,
+        price: this.list.length + 1
+      })
+    },
+    toggle () {
+      this.table = !this.table
+      console.log(this.table)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/a.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      {{ name }}
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      name: '我是组件'
+    }
+  }
+}
+
+</script>
