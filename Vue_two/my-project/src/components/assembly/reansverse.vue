3a1b39b911234cd86f7cadaff30024129719b763
小五 <wu.vkcyan@gmail.com>
2018-05-04 22:56:17 +0800

    完成单选组件

---
+++ Vue_two/my-project/src/components/assembly/reansverse.vue
@@ -0,0 +1,45 @@
+<template>
+   <div class="chooser-component">
+    <button v-for="item in ordernumber" :key="item" @click="index(item)" :class="newindex == item?'active':''">{{item}}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    ordernumber: {
+      type: Array
+    }
+  },
+  data () {
+    return {
+      newindex: 0
+    }
+  },
+  methods: {
+    index (index) {
+      this.newindex = index
+      this.$emit('onchange', index)
+    }
+  }
+}
+</script>
+
+<style>
+button {
+  display: inline-block;
+  border: 1px solid #e3e3e3;
+  height: 25px;
+  line-height: 25px;
+  padding: 0 8px;
+  margin-right: 5px;
+  border-radius: 3px;
+  text-align: center;
+  cursor: pointer;
+}
+.active {
+  border-color: #4fc08d;
+  background: #4fc08d;
+  color: #fff;
+}
+</style>
