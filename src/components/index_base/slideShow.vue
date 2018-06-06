2cf671f3762568663a08b42fb2ecd55ee0fb99a3
小五 <wu.vkcyan@gmail.com>
2018-06-06 13:20:04 +0800

    更变项目目录 ,解决之前轮播图的bug

--- src/components/index_base/slideShow.vue
+++ src/components/index_base/slideShow.vue
@@ -1,25 +1,25 @@
 <template>
   <div id="silde_show"  @mouseover="clearInv" @mouseout="runInv">
     <div class="slide_img">
-      <a href="slides[index].title">
-          <transition name="slide-trans">
-            <img :src="slides[newindex].src" v-if="isShow" alt="轮播图" title="这是轮播图">
+       <a :href="slides[newindex].href">
+         <transition name="slide-trans">
+            <img :src="slides[newindex].src" v-if="isShow">
           </transition>
-      </a>
-       <a href="slides[index].title">
           <transition name="slide-trans-old">
-            <img :src="slides[newindex].src" v-if="!isShow" alt="轮播图" title="这是轮播图">
+            <img :src="slides[newindex].src" v-if="!isShow">
           </transition>
       </a>
     </div>
-    <h2>{{ slides[newindex].title }}</h2>
-    <ul class="silde_pages">
-      <li @click="goto(prevIndex)">&lt;</li>
-      <li v-for="(item, index) in slides" :key="item.src" :class="{on: index === newindex}">
-        <a @click="goto(index)">{{ index+1 }}</a>
-      </li>
-      <li @click="goto(nextIndex)">&gt;</li>
-    </ul>
+    <div class="footer">
+      <h2>{{ slides[newindex].title }}</h2>
+      <ul class="silde_pages">
+        <li @click="goto(prevIndex)">&lt;</li>
+        <li v-for="(item, index) in slides" :key="item.src" :class="{on: index === newindex}">
+          <a @click="goto(index)">{{ index+1 }}</a>
+        </li>
+        <li @click="goto(nextIndex)">&gt;</li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -30,7 +30,8 @@ export default {
       type: Array
     },
     inv: {
-      type: Number
+      type: Number,
+      default: 1000
     }
   },
   data () {
@@ -59,8 +60,8 @@ export default {
     goto (index) {
       this.isShow = false
       setTimeout(() => {
-        this.newindex = index
         this.isShow = true
+        this.newindex = index
         this.$emit('onchange', index)
       }, 10)
     },
@@ -82,30 +83,41 @@ export default {
 <style scoped>
 #silde_show {
   width: 900px;
-  height: 350px;
+  height: 450px;
   position: relative;
   left: 200px;
   overflow: hidden;
 }
-#silde_show h2 {
+.footer {
   position: absolute;
   width: 100%;
   color: #fff;
   background: #000;
   opacity: 0.5;
-  bottom: 0px;
-  height: 30px;
-  text-align: left;
-  padding-left: 15px;
-  padding-top: 5px;
-  padding-bottom: -10px;
+  bottom: 0;
+  height: 35px;
 }
-.silde_pages {
+.footer h2 {
+  display: inline-block;
+  position: relative;
+  right: 420px;
+  top: 8px;
+  opacity: 0.8;
+}
+.footer ul {
+  bottom: 10px;
   position: absolute;
   z-index: 1000;
+  opacity: 0.8;
 }
 .slide_img {
-  height: 350px;
+  height: 450px;
+  position: absolute;
+}
+.slide_img img {
+  height: 450px;
+  width: 900px;
+  position: absolute;
 }
 .silde_pages {
   display: block;
@@ -135,15 +147,26 @@ export default {
 .on {
   background: red;
 }
-.slide-trans-enter-active {
+ .slide-trans-enter-active {
   transition: all 1s;
 }
 .slide-trans-enter {
-  transform: translateX(1800px);
+  transform: translateX(900px);
+}
+.slide-trans-old-leave-to {
+  transform: translateX(-900px);
 }
 .slide-trans-old-leave-active {
   transition: all 1s;
-  transform: translateX(-1800px);
 }
-
+/* .slide-trans-enter-active {
+  transition: all 2s;
+}
+.slide-trans-enter {
+  transform: translateX(900px);
+}
+.slide-trans-old-leave-active {
+  transition: all 2s;
+  transform: translateX(-900px);
+} */
 </style>
